a7c6a6c684f9a78a7b62acd4afb519328217655b
Unity3d-dev <67887068+Unity3d-dev@users.noreply.github.com>
2020-08-10 13:44:46 +0300

    Add files via upload

--- DebilniyCalcAlpha(0.0.3)/Helpmain.py
+++ DebilniyCalcAlpha(0.0.3)/Helpmain.py
@@ -2,13 +2,13 @@ import os
 import time
 mode = input("Выберите режим(+, -, *, /,): ")
 if mode == "+":
-    os.system("Start Details\Subtractions\LaunchPlus.py")
+    os.system("Start Details\Help\LaunchPlus.py")
 elif mode == "-":
-    os.system("Start Details\Subtractions\LaunchMinus.py")
+    os.system("Start Details\Help\LaunchMinus.py")
 elif mode == "*":
-    os.system("Start Details\Subtractions\LaunchMultiplication.py")
+    os.system("Start Details\Help\LaunchMultiplication.py")
 elif mode == "/":
-    os.system("Start Details\Subtractions\LaunchDivision.py")
+    os.system("Start Details\Help\LaunchDivision.py")
 else:
     print("Ошибка, пожалуйста подождите . . .")
     time.sleep(2)
